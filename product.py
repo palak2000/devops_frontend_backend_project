742144eb2503e2551bb72617e654cdb48682062a
Palak <palak2000.j@gmail.com>
2024-06-18 19:05:29 +0530

    com

--- product.py
+++ product.py
@@ -1,18 +1,17 @@
-from flask import Flask, Blueprint, render_template
-
+from flask import Flask, Blueprint, render_template, redirect, url_for
 product_bp = Blueprint('product', __name__)
 
 @product_bp.route('/')
 def product_home():
-    return render_template('product_home.html')
+    return render_template('product_details.html')
 
 @product_bp.route('/products/<product_id>')
 def product_details(product_id):
     product = {
         'product_id': product_id,
-        'product_name': 'Product Name',
-        'price': 100.0,
-        'description': 'Product Description'
+        'product_name': f'Product {product_id}',
+        'price': 100.0 * int(product_id),  # Dummy calculation for different prices
+        'description': f'Description of Product {product_id}'
     }
     return render_template('product_details.html', **product)
 
@@ -25,5 +24,4 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5001)
-
 
